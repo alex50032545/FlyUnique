6f08cebfd791bdf01236327b6a9d69be3876de8c
Alex <alex50032545@gmail.com>
2019-04-15 09:53:13 +0100

    x

---
+++ js.js
@@ -0,0 +1,18 @@
+
+function myFunction(id) {
+  var x = document.getElementById("demo");
+  x.style.color = "red";
+  $('.destinos').remove();
+  if (id == "europa")
+  {
+    alert("europe");
+  }
+  else if (id == "americanorte")
+  {
+    alert("norte");
+  }
+  else if (id == "americasul")
+  {
+    alert("sul");
+  }
+}
